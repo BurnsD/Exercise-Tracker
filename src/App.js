a046641223ccba9d2704ecb9def25c940d8f0c3d
BurnsD <Dustinburnsactech@gmail.com>
2022-04-10 22:53:40 -0500

    react connected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
-import Navbar from "./components/navbar.component"
+import Navbar from './components/navbar-component';
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
@@ -10,6 +10,7 @@ import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
+    
     <Router>
       <div className="container">
       <Navbar />
